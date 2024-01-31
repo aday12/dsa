e78fef712f6d3bd3659680315c5261ec925b8239
aday12 <aaronday1996@icloud.com>
2024-01-31 13:32:23 -0800

    Lab 1.5, Intersection tests

--- Lab1.5/test/com/algorithms/IntersectionTest.java
+++ Lab1.5/test/com/algorithms/IntersectionTest.java
@@ -1,13 +1,12 @@
 package com.algorithms;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-
 import java.util.Set;
 import java.util.function.BiFunction;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class IntersectionTest {
 
@@ -52,36 +51,73 @@ public class IntersectionTest {
 
     @Test
     public void intersection_identical() {
-        // TODO-Lab1.5: Implement test of intersection for arrays with all values in common, as
+        // Implement test of intersection for arrays with all values in common, as
         //  described in test-plan.md. You may invoke helper method testForSymmetricSuccess, if you
         //  find it useful to do so.
+        int[] nums1 = new int[]{0, 9, 7, 8, 6};
+        int[] nums2 = new int[]{7, 6, 9, 0, 8};
+        Set<Integer> expected = Set.of(0, 6, 7, 8, 9);
+        testForSymmetricSuccess(nums1, nums2, expected, intersection::intersection);
     }
 
     @Test
     public void intersectionFast_identical() {
-        // TODO-Lab1.5: Implement test of intersectionFast for arrays with all values in common, as
+        // Implement test of intersectionFast for arrays with all values in common, as
         //  described in test-plan.md. You may invoke helper method testForSymmetricSuccess, if you
         //  find it useful to do so.
+        int[] nums1 = new int[]{0, 9, 7, 8, 6};
+        int[] nums2 = new int[]{7, 6, 9, 0, 8};
+        Set<Integer> expected = Set.of(0, 6, 7, 8, 9);
+        testForSymmetricSuccess(nums1, nums2, expected, intersection::intersectionFast);
     }
 
     @Test
     public void intersection_empty() {
-        // TODO-Lab1.5: Implement test of intersection for arrays with no values in common, as
+        // Implement test of intersection for arrays with no values in common, as
         //  described in test-plan.md. You may invoke helper method testForSymmetricSuccess, if you
         //  find it useful to do so.
+        int[] nums1 = new int[]{2, 4, 1, 3, 5};
+        int[] nums2 = new int[]{7, 6, 9, 0, 8};
+        Set<Integer> expected = Set.of();
+        testForSymmetricSuccess(nums1, nums2, expected, intersection::intersectionFast);
     }
 
     @Test
     public void intersectionFast_empty() {
-        // TODO-Lab1.5: Implement test of intersectionFast for arrays with no values in common, as
+        // Implement test of intersectionFast for arrays with no values in common, as
         //  described in test-plan.md. You may invoke helper method testForSymmetricSuccess, if you
         //  find it useful to do so.
+        int[] nums1 = new int[]{2, 4, 1, 3, 5};
+        int[] nums2 = new int[]{7, 6, 9, 0, 8};
+        Set<Integer> expected = Set.of();
+        testForSymmetricSuccess(nums1, nums2, expected, intersection::intersection);
     }
 
-    // TODO-Lab1.5 (optional): Implement tests of intersection and intersectionFast, with a null
+
+    // Implement tests of intersection and intersectionFast, with a null
     //  value for one or the other (or both) of the arguments, and a NullPointerException being
     //  thrown. Try to take advantage of symmetry, and feel free to create a helper method to reduce
     //  duplicated code.
+
+    @Test
+    public void Intersection_null() {
+        int[] nums1 = null;
+        int[] nums2 = new int[]{7, 6, 9, 0, 8};
+        Set<Integer> expected = Set.of();
+        assertThrows(NullPointerException.class, () -> {
+            intersection.intersection(nums1, nums2);
+        });
+    }
+
+    @Test
+    public void IntersectionFast_null() {
+        int[] nums1 = null;
+        int[] nums2 = new int[]{7, 6, 9, 0, 8};
+        Set<Integer> expected = Set.of();
+        assertThrows(NullPointerException.class, () -> {
+            intersection.intersectionFast(nums1, nums2);
+        });
+    }
 
     private void testForSymmetricSuccess(int[] a, int[] b, Set<Integer> expected,
                                          BiFunction<int[], int[], Set<Integer>> invocation) {
