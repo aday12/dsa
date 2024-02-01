32f60d01abe1991aeca3868ac91b98119e2ebdc0
aday12 <aaronday1996@icloud.com>
2024-02-01 13:48:00 -0800

    Lab 2.2, bubble sort

--- Lab2.2/src/com/algorithms/BubbleSort.java
+++ Lab2.2/src/com/algorithms/BubbleSort.java
@@ -12,7 +12,27 @@ public class BubbleSort {
     }
 
     public static void sort(int[] numbers) {
-        // TODO-Lab2.2: implement bubble sort pseudocode
+        // implement bubble sort pseudocode
+
+        for (int k = 1; k < numbers.length; k++) {
+            boolean swapped = true;
+            for (int j = 0; j < numbers.length - 1 - (k - 1); j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    swapped = true;
+                    swap(numbers, j, j + 1);
+                } else {
+                    break;
+                }
+//                if (!swapped) {
+//                    break;
+//                }
+            }
+        }
     }
 
+    private static void swap(int[] nums, int p, int q) {
+        int temp = nums[p];
+        nums[p] = nums[q];
+        nums[q] = temp;
+    }
 }
--- Lab2.2/test/com/algorithms/BubbleSortTest.java
+++ Lab2.2/test/com/algorithms/BubbleSortTest.java
@@ -1,7 +1,26 @@
 package com.algorithms;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
 public class BubbleSortTest {
 
-    // TODO-Lab2.2: Create test methods for bubble sort implementation.
+    // Create test methods for bubble sort implementation.
+    int[] arr;
+    int[] control = new int[]{1, 3, 5, 7, 9};
 
+    @Before
+    public void setUp() throws Exception {
+        arr = new int[]{9, 7, 5, 3, 1};
+    }
+
+    @Test
+    public void sortSorts() {
+        BubbleSort.sort(arr);
+        assertArrayEquals(arr, control);
+    }
 }
